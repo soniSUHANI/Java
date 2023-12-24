736b8250702371a19151d5b8f9b52eff2a74a819
soni <114800693+soniSUHANI@users.noreply.github.com>
2023-12-24 19:52:40 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,20 @@
+public class FirstClass {
+    public static void main(String args[]){
+        //System.out.println("Hello World with java my class");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        //variables 
+        String name = "soni";
+        int age = 19;
+        int a = 25;
+        int b = 10;
+        int sum = a+b;
+        int diff = a-b;
+        int multi = a*b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(multi);
+    }
+}
